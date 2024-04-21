279a2eb5425d033f7268ac871bb85098e4bd3431
Tomás Pinho <me@tomaspinho.com>
2024-04-21 18:54:50 +0100

    feat: add created_at and updated_at timestamps to example

--- sql/migrations/20240420215139_examples.up.sql
+++ sql/migrations/20240420215139_examples.up.sql
@@ -1,5 +1,9 @@
 CREATE TABLE "examples" (
   id           BIGSERIAL PRIMARY KEY,
+
   name         text      NOT NULL,
-  description  text
+  description  text,
+
+  created_at TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP   DEFAULT CURRENT_TIMESTAMP
 );
